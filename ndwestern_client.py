2030a536a5ee01032610987d894ceb13bf99ae33
itachi1010 <onahben977@gmail.com>
2023-07-28 08:24:18 +0100

    Your commit message here

---
+++ ndwestern_client.py
@@ -0,0 +1,161 @@
+# import required modules
+import socket
+import threading
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import messagebox
+from plyer import notification
+import os
+
+
+
+
+# Retrieves the host's IP automatically
+HOST = socket.gethostbyname(socket.gethostname())
+
+if HOST == '127.0.0.1':
+    print('You are connected to your localhost (127.0.0.1), not to the LAN network')
+
+# Get the last 4 digits and convert them to an integer
+raw_port = ''.join(HOST.split('.')[2:])
+PORT = int(raw_port)
+
+# Make sure the PORT is within the permissible range (use a default port if not)
+if PORT<1024 or PORT>65535:
+    PORT = 1234  # default port
+
+LISTENER_LIMIT = 5
+active_clients = []
+active_threads = []  # List of active threads
+
+DARK_GREY = '#121212'
+MEDIUM_GREY = '#1F1B24'
+OCEAN_BLUE = '#464EB8'
+WHITE = "white"
+FONT = ("Helvetica", 17)
+BUTTON_FONT = ("Helvetica", 15)
+SMALL_FONT = ("Helvetica", 13)
+
+# Creating a socket object
+# AF_INET: we are going to use IPv4 addresses
+# SOCK_STREAM: we are using TCP packets for communication
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Function to show a toast notification
+def show_toast_notification(title, message):
+    notification.notify(
+        title=title,
+        message=message,
+        timeout=10
+    )
+
+def add_message(message):
+    message_box.config(state=tk.NORMAL)
+    message_box.insert(tk.END, message + '\n')
+    message_box.config(state=tk.DISABLED)
+    show_toast_notification("New Message", message)
+
+def connect():
+    try:
+        client.connect((HOST, PORT))
+        print("Successfully connected to server")
+        add_message("[SERVER] Successfully connected to the server")
+        show_toast_notification("Chat Update", "Successfully connected to the server")
+    except:
+        messagebox.showerror("Unable to connect to server", f"Unable to connect to server {HOST} {PORT}")
+
+    username = username_textbox.get()
+    if username != '':
+        client.sendall(username.encode())
+    else:
+        messagebox.showerror("Invalid username", "Username cannot be empty")
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client,)).start()
+
+    username_textbox.config(state=tk.DISABLED)
+    username_button.config(state=tk.DISABLED)
+
+def send_message():
+    message = message_textbox.get()
+    if message != '':
+        client.sendall(message.encode())
+        message_textbox.delete(0, len(message))
+    else:
+        messagebox.showerror("Empty message", "Message cannot be empty")
+
+# Client code
+def listen_for_messages_from_server(client):
+    while True:
+        data = client.recv(1024)
+        if data == b'':
+            desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')  # get the desktop path
+            file_path = os.path.join(desktop, 'local mail', 'alert.txt')  # set the file path to 'local mail' folder on the desktop
+            receive_file(client, file_path)
+        else:
+            message = data.decode('utf-8')
+            if message == '':
+                print("Server has closed the connection")
+                add_message("[SERVER] Server has closed the connection")
+                break
+            else:
+                if "~" in message:  # check if "~" is in the message
+                    username = message.split("~")[0]
+                    content = message.split('~')[1]
+                    add_message(f"[{username}] {content}")
+                    if content == "ALERT":
+                        show_toast_notification("Alert", "Check the received alert.txt file")
+                else:  # if "~" is not in the message, then it's the alert.txt file
+                    add_message("[SERVER] Received alert.txt file")
+
+def receive_file(client, file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)  # create 'local mail' folder if it doesn't exist
+    with open(file_path, 'wb') as file:
+        while True:
+            data = client.recv(1024)
+            if data == b'':
+                break
+            file.write(data)
+
+# main function
+def main():
+    root.mainloop()
+
+root = tk.Tk()
+root.geometry("600x600")
+root.title("ndwestern Client")
+root.resizable(False, False)
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=4)
+root.grid_rowconfigure(2, weight=1)
+
+top_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
+top_frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+middle_frame = tk.Frame(root, width=600, height=400, bg=MEDIUM_GREY)
+middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
+
+bottom_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
+bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
+
+username_label = tk.Label(top_frame, text="Enter username:", font=FONT, bg=DARK_GREY, fg=WHITE)
+username_label.pack(side=tk.LEFT, padx=10)
+
+username_textbox = tk.Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=23)
+username_textbox.pack(side=tk.LEFT)
+
+username_button = tk.Button(top_frame, text="Join", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=connect)
+username_button.pack(side=tk.LEFT, padx=15)
+
+message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=38)
+message_textbox.pack(side=tk.LEFT, padx=10)
+
+message_button = tk.Button(bottom_frame, text="Send", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
+message_button.pack(side=tk.LEFT, padx=10)
+
+message_box = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=WHITE, width=67, height=26.5)
+message_box.config(state=tk.DISABLED)
+message_box.pack(side=tk.TOP)
+
+if __name__ == '__main__':
+    main()
---
+++ ndwestern_server.py
@@ -0,0 +1,125 @@
+import socket
+import threading
+import concurrent.futures
+
+# Retrieves the host's IP automatically
+HOST = socket.gethostbyname(socket.gethostname())
+
+if HOST == '127.0.0.1':
+    print('You are connected to your localhost (127.0.0.1), not to the LAN network')
+
+# Get the last 4 digits and convert them to an integer
+raw_port = ''.join(HOST.split('.')[2:])
+PORT = int(raw_port)
+
+# Make sure the PORT is within the permissible range (use a default port if not)
+if PORT<1024 or PORT>65535:
+    PORT = 1234  # default port
+
+
+
+LISTENER_LIMIT = 5
+active_clients = []
+active_threads = []  # List of active threads
+
+
+# Rest of the code remains the same...
+
+# Function to listen for upcoming messages from a client
+# Server code
+def listen_for_messages(client, username):
+    while True:
+        try:
+            message = client.recv(2048).decode('utf-8')
+            if message == "QUIT":
+                send_message_to_client(client, "SERVER~QUIT")
+                remove_client(client, username)
+                break
+            elif message == "ALERT":
+                with open('alert.txt', 'r') as file:
+                    alert_message = file.read()
+                send_message_to_client(client, f"SERVER~{alert_message}")
+                send_file(client, 'alert.txt')
+            elif message:
+                final_msg = f"{username}~{message}"
+                send_messages_to_all(final_msg)
+            else:
+                print(f"The message sent from client {username} is empty")
+        except Exception as e:
+            print(f"Error receiving from {username}: {e}")
+            remove_client(client, username)
+            break
+
+# Function to send message to a single client
+def send_message_to_client(client, message):
+    try:
+        client.sendall(message.encode())
+    except Exception as e:
+        print(f"Error sending to client: {e}")
+        remove_client(client)
+# Function to send a file to a client
+
+
+
+def send_file(client, file_path):
+    with open(file_path, 'rb') as file:
+        while True:
+            data = file.read(1024)
+            if not data:
+                break
+            client.sendall(data)
+        client.sendall(b'')  # Send termination signal
+
+# Function to remove a client
+def remove_client(client, username=None):
+    for user in active_clients:
+        if user[1] == client:
+            active_clients.remove(user)
+            print(f"Client {username if username else 'unknown'} disconnected")
+            break
+
+# Function to send any new message to all the clients that
+# are currently connected to this server
+def send_messages_to_all(message):
+    for user in active_clients:
+        send_message_to_client(user[1], message)
+
+# Function to handle client
+def client_handler(client):
+    try:
+        # Server will listen for client message that will Contain the username
+        username = client.recv(2048).decode('utf-8')
+        if username:
+            active_clients.append((username, client))
+            prompt_message = "SERVER~" + f"{username} added to the chat"
+            send_messages_to_all(prompt_message)
+            listen_for_messages(client, username)
+        else:
+            print("Client username is empty")
+    except Exception as e:
+        print(f"Error handling client: {e}")
+    finally:
+        client.close()
+
+# Main function
+def main():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=LISTENER_LIMIT) as executor:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            try:
+                server.bind((HOST, PORT))
+                print(f"Running the server on {HOST} {PORT}")
+                server.listen(LISTENER_LIMIT)
+
+                # This while loop will keep listening to client connections
+                while True:
+                    client, address = server.accept()
+                    print(f"Successfully connected to client {address[0]} {address[1]}")
+                    active_threads.append(executor.submit(client_handler, client))
+            except Exception as e:
+                print(f"Server error: {e}")
+            finally:
+                for future in concurrent.futures.as_completed(active_threads):
+                    future.result()
+
+if __name__ == '__main__':
+    main()
